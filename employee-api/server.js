771d7f24756fb32e17d1137ed894b953582d02ad
William R. Ferreira <willian.usp@hotmail.com>
2021-04-08 11:17:20 -0300

    Criação da estrutura de pastas do projeto

---
+++ employee-api/server.js
@@ -0,0 +1,7 @@
+/** 
+ * arquivo: server.js
+ * descrição: arquivo responsável por toda a configuração e execução do Back-end ('Employee')
+ * data: 08/04/2021
+ * autor: William R. Ferreira
+ * 
+*/
